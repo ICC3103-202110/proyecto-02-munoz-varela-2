f4415c101321ef0d3ad0a5645428c36e97422868
JaMunioz <jamunoz6@miuandes.cl>
2021-05-30 21:44:21 -0400

    Seventh commit

--- update.js
+++ update.js
@@ -5,8 +5,14 @@ function fname(input1,input2,input3,input4,model){
         model.name_city.push(input2.input2)//pushing the city
         return model.name_city
     } else {
-        console.log(model.name_city.findIndex(name_city => name_city === "input3.input3"))
-        return model.name_city
+        if (input1 === 'Update city'){
+            return model.name_city
+        } else{
+            console.log(model.name_city) //lista
+            console.log(input4.input4) //elemento
+            model.name_city.splice(0,1)
+            return model.name_city
+        }
     }
 }
 function ftemp(input1,input2,input3,input4,model){
@@ -14,7 +20,12 @@ function ftemp(input1,input2,input3,input4,model){
         model.temp.push((Math.random() * (30 - 0) + 0).toFixed(2))//randomvalue
         return model.temp
     } else {
-        return model.temp
+        if (input1 === 'Update city'){
+            return model.temp
+        } else{
+            model.temp.splice(0,1)
+            return model.temp
+        }
     }
 }
 function fmax(input1,input2,input3,input4,model){
@@ -22,20 +33,29 @@ function fmax(input1,input2,input3,input4,model){
         model.max.push((Math.random() * (30 - 0) + 0).toFixed(2))//randomvalue
         return model.max
     } else {
-        return model.max
+        if (input1 === 'Update city'){
+            return model.max
+        } else{
+            model.max.splice(0,1)
+            return model.max
+        }
     }
 }
 function fmin(input1,input2,input3,input4,model){
     if (input1 === 'Add city'){
         model.min.push((Math.random() * (30 - 0) + 0).toFixed(2))//randomvalue
         return model.min
-    } else{
-        return model.min
+    } else {
+        if (input1 === 'Update city'){
+            return model.min
+        } else{
+            model.min.splice(0,1)
+            return model.min
+        }
     }
 }
 
 function update(input1,input2,input3,input4,model){
-    console.log(input2.input2)
     return {
         ...model,
         input1: input1,
